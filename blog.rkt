ee89172e544963b416a3469da62004ed17cab746
JP Verkamp <me@jverkamp.com>
2014-03-24 19:07:31 -0700

    Added support for plugins to register pre/post render events

--- blog.rkt
+++ blog.rkt
@@ -1,6 +1,7 @@
 #lang at-exp racket
 
-(require racket/runtime-path
+(require racket/date
+         racket/runtime-path
          "plugin.rkt"
          "post.rkt"
          "render.rkt")
@@ -31,11 +32,20 @@
     
     (define new-post (with-input-from-file path read-post))
     
+    (new-post "path" (path->string path))
+    
     (define attachment-path (regexp-replace #px"\\.htm" (path->string path) ""))
     (when (directory-exists? attachment-path)
       (new-post "files-path" attachment-path))
     
     new-post))
+
+(parameterize ([date-display-format 'iso-8601])
+  (set! posts (sort posts (λ (post1 post2)
+                            (and (post1 "date")
+                                 (post2 "date")
+                                 (string<? (date->string (post1 "date"))
+                                           (date->string (post2 "date"))))))))
 
 (printf "Formatting contents...\n")
 (for ([post (in-list posts)])
@@ -43,25 +53,28 @@
                                (printf "Failed in '~a': ~a\n"
                                        (post "title")
                                        (exn-message err)))])
-    (post "content" (render (post "content") #:environment plugins))))
+    (pre-render! post)
+    (post "content" (render (post "content") #:environment plugins))
+    (post-render! post)))
 
 (printf "Writing posts...\n")
 (system (format "rm -rf '~a'" output-path))
 (make-directory output-path)
 (for ([post (in-list posts)])
-  (define date @post{date})
-  (define path 
-    (build-path output-path
-                (~a (date-year date)  #:width 4 #:align 'right #:pad-string "0")
-                (~a (date-month date) #:width 2 #:align 'right #:pad-string "0")
-                (~a (date-day date)   #:width 2 #:align 'right #:pad-string "0")
-                (slugify @post{title})))
-  
-  (make-directory* path)
-  (when @post{files-path}
-    (for ([file (in-directory @post{files-path})])
-      (copy-file file (build-path path (file-name-from-path file)))))
-  
-  (with-output-to-file (build-path path "index.htm")
-    (thunk
-      (display @post{content}))))
+  (with-handlers ([exn? (λ (exn) (printf "Could not write '~a': ~a\n" (post "title") (exn-message exn)))])
+    (define date @post{date})
+    (define path 
+      (build-path output-path
+                  (~a (date-year date)  #:width 4 #:align 'right #:pad-string "0")
+                  (~a (date-month date) #:width 2 #:align 'right #:pad-string "0")
+                  (~a (date-day date)   #:width 2 #:align 'right #:pad-string "0")
+                  (slugify @post{title})))
+    
+    (make-directory* path)
+    (when @post{files-path}
+      (for ([file (in-directory @post{files-path})])
+        (copy-file file (build-path path (file-name-from-path file)))))
+    
+    (with-output-to-file (build-path path "index.htm")
+      (thunk
+       (display @post{content})))))
--- plugin.rkt
+++ plugin.rkt
@@ -1,21 +1,41 @@
 #lang racket
 
 (provide load-plugins
-         plugins)
+         plugins
+         pre-render!
+         post-render!)
 
-(require racket/runtime-path)
+(require racket/runtime-path
+         "render.rkt")
 
 (define-runtime-path plugin-path "_plugins")
+
+; Plugins that are called before the renderer has run (content is a ScribbleHTML string)
+(define pre-render-listeners '())
+(define (pre-render! post)
+  (for ([listener (in-list pre-render-listeners)])
+    (listener post)))
+
+; Plugins that are called after the renderer has run (content is an HTML string)
+(define post-render-listeners '())
+(define (post-render! post)
+  (for ([listener (in-list post-render-listeners)])
+    (listener post)))
 
 ; Eventually this will be a hash of plugin name -> value
 (define plugins #f)
 
 ; This will be available to plugins in order to register them to our system
-(define (register-plugin name function)
+(define (register-plugin name function
+                         #:pre-render  [pre-render #f]
+                         #:post-render [post-render #f])
   (when (hash-has-key? plugins name)
     (error 'register-plugin "Duplicate plugin detected: ~a" name))
   (printf "  Plugin registered: ~a\n" name)
-  (hash-set! plugins name function))
+  (hash-set! plugins name function)
+  
+  (when pre-render (set! pre-render-listeners (cons pre-render pre-render-listeners)))
+  (when post-render (set! post-render-listeners (cons post-render post-render-listeners))))
 
 ; Load all racket files in the _plugins directory
 (define (load-plugins)
@@ -24,8 +44,14 @@
     (for ([file (in-directory plugin-path)]
           #:when (regexp-match #px".rkt$" file))
       (parameterize ([current-namespace (make-base-namespace)])
+        ; Allow plugin files to register themselves
         (eval `(define register-plugin ,register-plugin))
+        
+        ; Allow plugins access to the rendering function
+        (eval `(define render ,render))
+        
+        ; Read and evaluate each expression in the plugin file in turn
         (with-input-from-file file
           (thunk
-            (for ([sexp (in-list (port->list))])
-              (eval sexp))))))))
+           (define sexp `(let () ,@(for/list ([sexp (in-list (port->list))]) sexp)))
+           (eval sexp)))))))

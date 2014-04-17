316f456d7586d3d3af174f52920052cbfb9b5dbe
JP Verkamp <me@jverkamp.com>
2014-04-16 22:17:33 -0700

    Misplaced unquote (in demo version anyways...)

--- _plugins/flickr.rkt
+++ _plugins/flickr.rkt
@@ -1,9 +1,9 @@
 (define (flickr-gallery id)
-  `(div (span (format "TODO: flickr-gallery for ~a" ,id))))
+  `(div (span ,(format "TODO: flickr-gallery for ~a" id))))
 
 (register-plugin 'flickr-gallery flickr-gallery)
 
 (define (flickr-image width height id #:title title)
-  `(div (span (format "TODO: flickr-image for ~a @ ~a" ,title ,id))))
+  `(div (span ,(format "TODO: flickr-image for ~a @ ~a" title id))))
 
 (register-plugin 'flickr-image flickr-image)

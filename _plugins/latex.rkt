a625f3c97d5719ca3abf82c56073dac365b63b45
JP Verkamp <me@jverkamp.com>
2014-06-12 23:36:06 -0700

    Added MathJax for LaTeX support
    All differentiated inline vs block LaTex:
    @latex{ ... } will be a block
    @latex['inline]{ ... } will be inline
    @latex['block]{ ... } will be a block

--- _plugins/latex.rkt
+++ _plugins/latex.rkt
@@ -1,4 +1,10 @@
 (define (latex . body*)
-  `(span "$$" ,(apply string-append body*) "$$"))
+  (match body*
+    [`(inline . ,body*)
+     `(span "\\\\(" ,(apply string-append body*) "\\\\)")]
+    [`(block . ,body*)
+     `(div "$$" ,(apply string-append body*) "$$")]
+    [any ; default to block mode
+     `(div "$$" ,(apply string-append body*) "$$")]))
 
 (register-plugin 'latex latex)

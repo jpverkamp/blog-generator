b1be2c51711679e0fd415f92b6cb9cd076dc68a5
JP Verkamp <me@jverkamp.com>
2014-06-15 17:12:34 -0700

    Added date based 'archive' category automatically to each post
    Note: Need to mod rewrite {yyyy}/{mm}/{dd} style links to these pages

--- _plugins/nested-categories.rkt
+++ _plugins/nested-categories.rkt
@@ -6,6 +6,13 @@
   (set! categories (make-hash))
   (set! site-ref site)
   
+  ; Add date based 'categories' to each post
+  (define (0pad thing width) (~a thing  #:width width #:align 'right #:pad-string "0"))
+  (for ([post (in-list (site "posts"))] #:when (post "date"))
+    (define date (post "date"))
+    (define ymd-category (format "Archives/~a/~a" (0pad (date-year date) 4) (0pad (date-month date) 2)))
+    (post "categories" (cons ymd-category (or (post "categories") '()))))
+  
   ; Create a nested category hash with a key *posts* at each level listing all posts
   (for ([post (in-list (site "posts"))] #:when (post "categories"))
     (for ([category (in-list (post "categories"))])
@@ -24,12 +31,8 @@
       (new-post "disable-markdown" #t)
       (new-post "template" "category-list")
       (new-post "permalink" (string-join (cons "category" (map slug (string-split path "/"))) "/"))
-      
       (new-post "template" "post")
       (new-post "content" "@generate-category-listing{@post{category}}")
-      
-      #;(new-post "template" "category-list")
-      #;(new-post "content" "")
       
       (new-post "category" path)
       (site "posts" (cons new-post (site "posts"))))

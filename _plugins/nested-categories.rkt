f1373041ddc35513c272796b8e8591cbe00d1420
JP Verkamp <me@jverkamp.com>
2014-06-25 20:08:36 -0400

    A fair bit of refactoring on nested categories
    - Split the functions that add subcategories and post listings
    - Rewrite arguments so they can be either keywords or not
    - Generate category lists after everything is done
    - Probably other things

--- _plugins/nested-categories.rkt
+++ _plugins/nested-categories.rkt
@@ -1,6 +1,8 @@
 (define site-ref #f)
 (define categories #f)
 
+(define (snoc x ls) (reverse (cons x (reverse ls))))
+
 ; Add categories to each post, assuming they are nested with / delims
 (define (add-nested-categories! site)
   (set! categories (make-hash))
@@ -25,10 +27,14 @@
       (new-post "template" "category-list")
       (new-post "permalink" (string-join (cons "category" (map slug (string-split path "/"))) "/"))
       (new-post "template" "post")
-      (new-post "content" "@generate-category-listing{@post{category}}")
+      (new-post "content" "
+@subcategories{@post{category}}
+
+<h2>Posts</h2>
+@post-list['include-post 5]{@post{category}}")
       
       (new-post "category" path)
-      (site "posts" (cons new-post (site "posts"))))
+      (site "posts" (snoc new-post (site "posts"))))
     
     (for ([category (in-list (hash-keys h))] #:when (string? category))
       (loop (hash-ref h category (hash))
@@ -56,9 +62,8 @@
 
 (register-plugin 'nested-category-navbar generate-navbar)
 
-; Add a function to generate a listing of all posts in a given (potentially nested) category
-; Possible values for sort-by: 'date 'title 'shuffle
-(define (generate-category-listing category #:sort-by [sort-by 'date] #:sort-ascending [sort-ascending? #t])
+; List the parent category and all subcategories for a given (potentially nested) cateogry
+(define (subcategories [category ""])
   (set! category (string-trim category "/"))
   
   (define parent-cateogry
@@ -66,12 +71,61 @@
       (and (> (length parts) 1)
            (cadr (reverse parts)))))
   
+  ; Get the posts for the category we're talking about
   (define cathash 
     (for/fold ([h categories]) ([part (in-list (string-split category "/"))])
       (hash-ref h part (make-hash))))
-  
   (define post-list (set->list (hash-ref cathash '*posts* (set))))
   
+  `(ul ((class "list-inline"))
+       ,@(if parent-cateogry
+             `((li "[↑ " (a ((href "..")) ,parent-cateogry) "]"))
+             `())
+       ,@(if (> (hash-count cathash) 1)
+             (for/list ([subcat (in-list (sort (filter string? (hash-keys cathash)) string<?))])
+               `(li "[" (a ((href ,(slug subcat))) ,subcat) "]"))
+             `())))
+
+(register-plugin 'subcategories subcategories)
+
+; Generate a listing of all posts in a given (potentially nested) category
+; args:
+; 'limit {number} - return only this many posts
+; 'sort-by {date, title, shuffle} - sort the returned posts (before applying limit)
+; 'sort-(ascending/descending) - sort by key ascending, ignored in shuffle
+; 'include-post - include the post along with the content
+(define (post-list #:limit                 [limit #f]
+                   #:sort-by               [sort-by 'date] 
+                   #:sort-ascending        [sort-ascending? #f] 
+                   #:include-post          [include-post #f]
+                   . args)
+  (define category 
+    (let loop ([args args])
+      (match args
+        [`(limit ,n . ,rest)        (set! limit n)            (loop rest)]
+        [`(sort-by ,how . ,rest)    (set! sort-by how)        (loop rest)]
+        [`(sort-ascending . ,rest)  (set! sort-ascending? #t) (loop rest)]
+        [`(sort-descending . ,rest) (set! sort-ascending? #f) (loop rest)]
+        [`(include-post 
+           ,(? number? n) . ,rest)  (set! include-post n)     (loop rest)]
+        [`(include-post . ,rest)    (set! include-post #t)    (loop rest)]
+        [`(,category)               category]
+        [`()                        ""])))
+  
+  (set! category (string-trim category "/"))
+  
+  (define parent-cateogry
+    (let ([parts (string-split (string-trim category "/") "/")])
+      (and (> (length parts) 1)
+           (cadr (reverse parts)))))
+  
+  ; Get the posts for the category we're talking about
+  (define cathash 
+    (for/fold ([h categories]) ([part (in-list (string-split category "/"))])
+      (hash-ref h part (make-hash))))
+  (define post-list (set->list (hash-ref cathash '*posts* (set))))
+  
+  ; Sort the posts, by date, title, random, or default (which should also be date)
   (define sorted-post-list 
     ((if sort-ascending? identity reverse)
      (case sort-by
@@ -88,21 +142,29 @@
         (sort post-list (λ (post1 post2) (zero? (random 2))))]
        [else post-list])))
   
-  `(div
-    (ul ((class "list-inline"))
-        ,@(if parent-cateogry
-              `((li "[↑ " (a ((href "..")) ,parent-cateogry) "]"))
-              `())
-        ,@(if (> (hash-count cathash) 1)
-              (for/list ([subcat (in-list (sort (filter string? (hash-keys cathash)) string<?))])
-                `(li "[" (a ((href ,(slug subcat))) ,subcat) "]"))
-              `()))
-    (h2 "Posts")
-    ,(parameterize ([date-display-format 'iso-8601])
-       `(ul ((class "list-unstyled"))
-            ,@(for/list ([post (in-list sorted-post-list)])
-                `(li "[" ,(date->string (post "date")) "] "
-                     (a ((href ,(string-append (or (site-ref "url") "") "/" (post "permalink")))) ,(post "title"))))))))
+  ; Apply the limit (if it exists); make sure that if we have fewer than that we return everything
+  (define final-post-list
+    (if limit
+        (take sorted-post-list (min (length sorted-post-list) limit))
+        sorted-post-list))
+  
+  ; Posts within this category or subcategories
+  (parameterize ([date-display-format 'iso-8601])
+    `(ul ((class "list-unstyled"))
+         ,@(for/list ([post (in-list final-post-list)]
+                      [i (in-naturals)])
+             `(li 
+               ,@(cond
+                   [(or (eq? include-post #t)
+                        (and (number? include-post)
+                             (< i include-post)))
+                    `((h1 ((class "entry-title")) (a ((href ,(string-append (or (site-ref "url") "") "/" (post "permalink")))) ,(post "title")))
+                      (span ((class "posted-on")) "[" ,(date->string (post "date")) "]")
+                      (div ,(post "more"))
+                      (hr))]
+                   [else
+                    `((span ((class "posted-on")) "[" ,(date->string (post "date")) "] ")
+                      (a ((href ,(string-append (or (site-ref "url") "") "/" (post "permalink")))) ,(post "title")))]))))))
 
-(register-plugin 'generate-category-listing generate-category-listing)
+(register-plugin 'post-list post-list)
 

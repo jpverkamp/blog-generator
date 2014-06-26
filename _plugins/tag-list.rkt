a8731a11d46129f0efe35e6702f22fd403162d73
JP Verkamp <me@jverkamp.com>
2014-06-26 18:26:43 -0400

    Added tags based on first and last level of categories

---
+++ _plugins/tag-list.rkt
@@ -0,0 +1,37 @@
+(define my-site #f)
+
+(define (add-tags site)
+  (set! my-site site)
+  
+  ; Add tags for primary and last posts
+  (for ([post (in-list (site "posts"))] #:when (post "categories"))
+    (for ([category (in-list (post "categories"))])
+      ; Don't generate tags for archive links
+      (when (not (regexp-match #px"^Archive" category))
+        ; Top level tags
+        (define tl-tag (last (string-split category "/")))
+        (define tl-tag-permalink (string-join (cons "category" (map slug (string-split category "/"))) "/"))
+        (post "tags" (hash-set (or (post "tags") (hash)) tl-tag tl-tag-permalink))
+  
+        ; Add a tag link for the last level of the category to the category permalink
+        (define ll-tag (first (string-split category "/")))
+        (define ll-tag-permalink (string-join (list "category" (slug (first (string-split category "/")))) "/"))
+        (post "tags" (hash-set (or (post "tags") (hash)) ll-tag ll-tag-permalink))))))
+
+(register-pre-all-plugin 'add-tags add-tags #:after 'nested-categories)
+
+; Generate a list of tags
+(define (tag-list tags)
+  (xexpr->string
+   (match tags
+     [#f ""]
+     [(? hash?)
+      `(ul ((class "tag-list list-inline"))
+           ,@(for/list ([(tag permalink) (in-hash tags)])
+               `(li (a ((href ,(string-append (or (my-site "url") "") "/" permalink))) ,tag))))]
+     [(? list?)
+      `(ul ((class "tag-list list-inline"))
+           ,@(for/list ([tag (in-list tags)])
+               `(li (a ((href ,(string-append (or (my-site "url") "") "/tag/" (slug tag)))) ,tag))))])))
+
+(register-plugin 'tag-list tag-list)  

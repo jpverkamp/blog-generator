974c0f0a600af692b50469d49be8ab9f24c224d5
JP Verkamp <me@jverkamp.com>
2014-07-26 17:39:58 -0700

    Improved embed using absolute paths

--- _plugins/embed.rkt
+++ _plugins/embed.rkt
@@ -1,13 +1,25 @@
 (define my-site #f)
 (define my-post #f)
 
-(define (embed src)
+(define (embed . args)
+  (define classes 
+    (cond
+      [(> (length args) 1)
+       (string-join (map ~a (reverse (cdr (reverse args)))) " ")]
+      [else
+       #f]))
+  
+  (define src
+    (last args))
+  
   (define absolute-path (~a (or (my-site "url") "/") "/" (or (my-post "permalink") ".") "/" src))
   (cond
     [(regexp-match #px"\\.(png|jpg|jpeg|gif)$" src)
-     `(a ((href ,absolute-path)) (img ((src ,absolute-path))))]
+     `(a ((href ,absolute-path) ,@(if classes `((class classes)) `())) 
+         (img ((src ,absolute-path))))]
     [else
-     `(a ((href ,absolute-path)) ,src)]))
+     `(a ((href ,absolute-path) ,@(if classes `((class classes)) `()))
+         ,src)]))
 
 (register-pre-render-plugin 
  'embed-get-post
@@ -15,5 +27,4 @@
    (set! my-post post)
    (set! my-site site)))
    
-
 (register-plugin 'embed embed)

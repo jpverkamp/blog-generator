b427e29d7c4ac69f585c25732f1a4882c8770743
JP Verkamp <me@jverkamp.com>
2014-07-27 23:51:05 -0700

    Remove single flickr images for the moment, hardcode for now

--- _plugins/flickr.rkt
+++ _plugins/flickr.rkt
@@ -4,8 +4,3 @@
          (data-per-page "30"))))
 
 (register-plugin 'flickr-gallery flickr-gallery)
-
-(define (flickr-image width height id #:title title)
-  `(div (span ,(format "TODO: flickr-image for ~a @ ~a" title id))))
-
-(register-plugin 'flickr-image flickr-image)

aeff5ce499b220df38ebd5aae49a295d2c08002f
JP Verkamp <me@jverkamp.com>
2014-07-27 21:53:48 -0700

    Fix latex plugin to actually generate inline that works with MathJax

--- _plugins/latex.rkt
+++ _plugins/latex.rkt
@@ -1,7 +1,7 @@
 (define (latex . body*)
   (match body*
     [`(inline . ,body*)
-     `(span "\\\\(" ,(apply string-append body*) "\\\\)")]
+     `(span "\\(" ,(apply string-append body*) "\\)")]
     [`(block . ,body*)
      `(div "$$" ,(apply string-append body*) "$$")]
     [any ; default to block mode

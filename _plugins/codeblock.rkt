3eb7d93c5a633e61ea5606db9ff0242828df99a8
JP Verkamp <me@jverkamp.com>
2014-07-29 23:50:32 -0700

    Automatically convert  to code tags

--- _plugins/codeblock.rkt
+++ _plugins/codeblock.rkt
@@ -5,6 +5,13 @@
   `(pre ((class ,lang))
         (code ,@body*)))
 
-; ((class ,(format "prettyprint linenums lang-~a" lang))) 
-
 (register-plugin 'codeblock codeblock)
+
+(define (code-ify-backticks post site)
+  (when (post "content")
+    (post "content" 
+          (regexp-replace* #px"`([^\n`]+)`" 
+                           (post "content") 
+                           (λ (_ text) (~a "<code>" text "</code>"))))))
+
+(register-pre-render-plugin 'code-ify-backticks code-ify-backticks)

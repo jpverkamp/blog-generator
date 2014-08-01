ace3281a3bfd3f131c9337af8b7860174196fcfa
JP Verkamp <me@jverkamp.com>
2014-07-31 23:40:55 -0700

    Update disqus plugin to use global site/post (and fix posts with apos in titles

--- _plugins/disqus.rkt
+++ _plugins/disqus.rkt
@@ -1,11 +1,11 @@
-(define (disqus site post)
+(define (disqus)
   (when (post "comments")
     (format 
    "<div id=\"disqus_thread\"></div>
 <script type=\"text/javascript\">
-var disqus_shortname = '~a';
-var disqus_title = '~a';
-var disqus_url = '~a';
+var disqus_shortname = \"~a\";
+var disqus_title = \"~a\";
+var disqus_url = \"~a\";
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
@@ -16,7 +16,7 @@ var disqus_url = '~a';
 <noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
 <a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>" 
    (or (site "disqus-shortname") (site "url"))
-   (post "title")
+   (regexp-replace* "\"" (post "title") "&quot;")
    (~a #;(site "url") "http://blog.jverkamp.com" "/" (post "permalink") "/"))))
 
 (register-plugin 'disqus disqus)
